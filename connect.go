ca173340d6dae37c99993f001a5d2cfd7becafb2
Lorina Poland <lorinapoland@gmail.com>
2021-11-05 12:26:10 -0700

    fix prints

--- connect.go
+++ connect.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"os"
 
@@ -9,34 +10,37 @@ import (
 	pb "github.com/stargate/stargate-grpc-go-client/stargate/pkg/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var stargateClient *client.StargateClient
 
 func main() {
 
-	grpcEndpoint := "localhost:8090"
-	authEndpoint := "localhost:8081"
+	// grpcEndpoint := "localhost:8090"
+	// authEndpoint := "localhost:8081"
 
-	conn, err := grpc.Dial(grpcEndpoint, grpc.WithInsecure(), grpc.WithBlock(),
-		grpc.WithPerRPCCredentials(
-			auth.NewTableBasedTokenProviderUnsafe(
-				fmt.Sprintf("http://%s/v1/auth", authEndpoint), "cassandra", "cassandra",
-			),
-		),
-	)
-
-	// grpcEndpoint := "a2b4465c-e7a4-4cb7-a4a4-c829f0ef10d6-us-west1.apps.astra.datastax.com:443"
-
-	// config := &tls.Config{
-	// 	InsecureSkipVerify: false,
-	// }
-
-	// conn, err := grpc.Dial(grpcEndpoint, grpc.WithTransportCredentials(credentials.NewTLS(config)), grpc.WithBlock(),
+	// conn, err := grpc.Dial(grpcEndpoint, grpc.WithInsecure(), grpc.WithBlock(),
 	// 	grpc.WithPerRPCCredentials(
-	// 		auth.NewStaticTokenProvider("AstraCS:uuwizlOZhGxrUxaOqHPLAGCK:b4296e99a9f801d78043272b0efd79dca115b1fd95765780df36ed3ada87ff9b"),
+	// 		auth.NewTableBasedTokenProviderUnsafe(
+	// 			fmt.Sprintf("http://%s/v1/auth", authEndpoint), "cassandra", "cassandra",
+	// 		),
 	// 	),
 	// )
+
+	grpcEndpoint := "ff1d03c6-aaae-440c-b463-4508b2993d0f-us-east1.apps.astra-dev.datastax.com:443"
+	// grpcEndpoint := "a2b4465c-e7a4-4cb7-a4a4-c829f0ef10d6-us-west1.apps.astra.datastax.com:443"
+
+	config := &tls.Config{
+		InsecureSkipVerify: false,
+	}
+
+	conn, err := grpc.Dial(grpcEndpoint, grpc.WithTransportCredentials(credentials.NewTLS(config)), grpc.WithBlock(),
+		grpc.WithPerRPCCredentials(
+			// auth.NewStaticTokenProvider("AstraCS:uuwizlOZhGxrUxaOqHPLAGCK:b4296e99a9f801d78043272b0efd79dca115b1fd95765780df36ed3ada87ff9b"),
+			auth.NewStaticTokenProvider("AstraCS:qTvPRvZTLygyfEZynXIRcOrs:cbd02f55e76e6e948673e8127bbf81feeb6a5564ccfe1f513cf73b9409cf70ed"),
+		),
+	)
 
 	stargateClient, err = client.NewStargateClientWithConn(conn)
 
@@ -44,23 +48,19 @@ func main() {
 		fmt.Printf("error creating client %v", err)
 		os.Exit(1)
 	}
-	// if err != nil {
-	// 	fmt.Printf("error dialing connection %v", err)
-	// 	os.Exit(1)
-	// }
 	// defer conn.Close()
 	fmt.Printf("made client\n")
 
 	// Create a new keyspace
-	createKeyspaceStatement := &pb.Query{
-		Cql: "CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};",
-	}
-
-	_, err = stargateClient.ExecuteQuery(createKeyspaceStatement)
-	// if err != nil {
-	// 	return err
+	// createKeyspaceStatement := &pb.Query{
+	// 	Cql: "CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};",
 	// }
-	fmt.Printf("made keyspace\n")
+
+	// _, err = stargateClient.ExecuteQuery(createKeyspaceStatement)
+	// // if err != nil {
+	// // 	return err
+	// // }
+	// fmt.Printf("made keyspace\n")
 
 	// Create a new table
 	createTableQuery := &pb.Query{
@@ -99,49 +99,18 @@ func main() {
 
 	result := response.GetResultSet()
 
-	// NOT WORKING
-	// var i, j int
-	// for i = 0; i < 2; i++ {
-	// 	for j = 1; j < 2; j++ {
-	// 		firstname, err := client.ToString(result.Rows[i].Values[i])
-	// 		if err != nil {
-	// 			fmt.Printf("error getting firstname %v", err)
-	// 			os.Exit(1)
-	// 		}
-	// 		lastname, err := client.ToString(result.Rows[i].Values[j])
-	// 		if err != nil {
-	// 			fmt.Printf("error getting lastname %v", err)
-	// 			os.Exit(1)
-	// 		}
-	// 		fmt.Printf("%v %v \n", firstname, lastname)
-	// 	}
-	// }
-
-	firstname, err := client.ToString(result.Rows[0].Values[0])
-	if err != nil {
-		fmt.Printf("error getting firstname %v", err)
-		os.Exit(1)
+	var i, j int
+	for i = 0; i < 2; i++ {
+		valueToPrint := ""
+		for j = 0; j < 2; j++ {
+			value, err := client.ToString(result.Rows[i].Values[j])
+			if err != nil {
+				fmt.Printf("error getting value %v", err)
+				os.Exit(1)
+			}
+			valueToPrint += " "
+			valueToPrint += value
+		}
+		fmt.Printf("%v \n", valueToPrint)
 	}
-
-	lastname, err := client.ToString(result.Rows[0].Values[1])
-	if err != nil {
-		fmt.Printf("error getting lastname %v", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("%v %v \n", firstname, lastname)
-
-	firstname2, err := client.ToString(result.Rows[1].Values[0])
-	if err != nil {
-		fmt.Printf("error getting firstname %v \n", err)
-		os.Exit(1)
-	}
-
-	lastname2, err := client.ToString(result.Rows[1].Values[1])
-	if err != nil {
-		fmt.Printf("error getting lastname %v \n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("%v %v \n", firstname2, lastname2)
 }
